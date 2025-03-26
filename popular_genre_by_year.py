570f190b3c859380bc18757321456d0736535087
NSUUF <csmnsuuf@ljmu.ac.uk>
2025-03-26 12:03:25 +0000

    updated

--- popular_genre_by_year.py
+++ popular_genre_by_year.py
@@ -2,7 +2,7 @@ import pandas as pd #import pandas for data extraction
 
 data = pd.read_csv('vgsales.csv') #reads and loads video game sales file content
 
-unique_years = data["Year"].dropna().unique() # extracts unique years, not including games that have 'NA' 
+unique_years = data['Year'].dropna().unique() # extracts unique years, not including games that have 'NA' 
 year = unique_years.astype(int) #converts year data into int so we can form comparisons
 year.sort() #sort function to sort year data in order from lowest to highest
 
@@ -13,18 +13,15 @@ chosenYear = input('\nPlease select a year from the list above to view the most 
 chosenYear = int(chosenYear) #converts input from string to int so that year data and input data can be compared
 
 if chosenYear in year:
-    year_data = data[data["Year"]== chosenYear] #filters and finds user input match with 'Year'
+    year_data = data[data['Year']== chosenYear] #filters and finds user input match with 'Year'
     
    
-    global_sales = year_data['Global_Sales'] 
-    # max_globalsales = max(global_sales)
-
-    # genre = year_data[year_data['Global_Sales']] == max_globalsales]['Genre'].values[0]
-    # print(genre)
-  
-    print(f"\n Highest global sales for that year: {max(global_sales)}") #produces max value within the global sales column that meet the year condition
+    global_sales = year_data['Global_Sales'] #filters through global sales column and only extracts and stores 
+    max_globalsales = max(global_sales) #calculates and stores max value in global sales
+    pop_genre = year_data.loc[year_data['Global_Sales'] == max_globalsales, 'Genre'] #finds the genre that matches the max global sale found (same row diff column)
+   
+    print(f"The most popular genre for that year is: {pop_genre}")
     
-    #have to still find genre match and print that
 else:
   print('\nInvalid input.')
 
@@ -37,7 +34,9 @@ else:
       year_data = data[data["Year"]== chosenYear]
     
       global_sales = year_data['Global_Sales']
-    
-      print(f"\n{global_sales}")
-      print(f"Highest global sales for year: {max(global_sales)}")
+      max_globalsales = max(global_sales)
+      pop_genre = year_data.loc[year_data['Global_Sales'] == max_globalsales, 'Genre']
+
+  print(f"The most popular genre for that year is: {pop_genre}")
+   
 
