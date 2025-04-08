0e6e67bf482afa8768344d47f43cb1fd9aedcaea
Ameerah Zubairu <csmazuba@ljmu.ac.uk>
2025-04-08 13:59:08 +0100

    third commit

--- Ameerah Zubairu-visualisation.py
+++ Ameerah Zubairu-visualisation.py
@@ -6,8 +6,8 @@ dataSet= pd.read_csv('vgsales.csv')
 
 OverallGames= dataSet["Genre"].value_counts()
 Games= dataSet["Genre"]
-games = ["sports", "Plaform", "Racing", "Role-Playing", "Puzzle", "Misc", "Shooter", "Simulation", "Action", "Fighting", "Adventure", "Strategy"]
+games = ["Action", "Sports", "Misc", "Role-Playing", "Shooter", "Adventure", "Racing", "Platform", "Simulation", "Fighting", "Strategy", "Puzzle"]
 myexplode = [0,0,0,0.3,0,0,0,0,0,0,0,0,]
-mycolors = ["Red", "black", "blue", "purple", "pink", "grey", "green", "yellow", "orange", "hotpink", "lightgreen", "indigo"]
-plt.pie(OverallGames, labels = games, explode = myexplode, shadow = True, colors= mycolors)
+mycolors = ["Red", "lightblue", "blue", "purple", "pink", "grey", "green", "yellow", "orange", "hotpink", "lightgreen", "indigo"]
+plt.pie(OverallGames, labels = games, explode = myexplode, shadow = True, colors= mycolors, autopct="%.2f%%")
 plt.show()
